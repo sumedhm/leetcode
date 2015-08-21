72d2a6257f3e08e11a1182170b928e9cbf0cda22
sumedhm <sumedhm18@gmail.com>
2015-08-21 17:34:26 +0530

    322

--- codes/longestvalidparantheses.cpp
+++ codes/longestvalidparantheses.cpp
@@ -6,75 +6,31 @@
 using namespace std;
 
 int longestValidParentheses(string s) {
-        stack<char> st;
-        st.push('$');
-        char c;
-        int max=0, cur=0;
+	if(s.length()<=1) return 0;
+        stack<int> st;
+        int max=0, cur=0, valid=-1;
         for(int i=0;i<s.length();i++){
-        	cur = 0;
         	if(s[i]=='('){
-        		st.push(s[i]);
+        		st.push(i);
         	} else if(s[i]==')'){
         		if(st.empty()){
-        			//do nothing
+	        		valid = i;
         		} else {
-        			c = st.top();
-        			if(c=='$' || c==')') continue;
-        			if(c=='('){
-        				st.pop();     
-        				cur = 1;  
-        				c = st.top();
-        				while(c!='$' && c!='(' && c!=')'){
-        					st.pop();
-        					cur += c-'0';
-        					c = st.top();
-        				}
-        				st.push(cur+'0');
-        				if(max<cur) max = cur;
+        			st.pop();
+        			if(st.empty()){
+        				cur = i - valid;
+        				if(max < cur) max = cur;
         			} else {
-        				st.pop();
-        				cur = c-'0';
-        				c = st.top();
-        				while(c!='$' && c!='(' && c!=')'){
-        					st.pop();
-        					cur += c-'0';
-        					c = st.top();
-        				}
-        				if(c=='('){
-        					cur++;
-        					st.pop();
-        				} else {
-        					st.push(')');
-        					if(max < cur) max = cur;
-        					cur = 0;
-        					continue;
-        				}
-        				c = st.top();
-        				while(c!='$' && c!='(' && c!=')'){
-        					st.pop();
-        					cur += c-'0';
-        					c = st.top();
-        				}
-        				st.push(cur+'0');
-        				if(max<cur) max = cur;
+        				cur = i - st.top();
+        				if(max <cur) max = cur;
         			}
         		}
         	}
         }
-        // c = st.top();
-        // while(c!='$'){
-        // 	st.pop();
-        // 	if(c=='(') break;
-        // 	else{
-        // 		cur += c-'0';
-        // 	}
-        // 	c = st.top();
-        // }
-        if(max<cur) max = cur;
-        return 2*max;
+        return max;
 }
 
 int main(){
-	cout << longestValidParentheses(")(())))(())())") << endl;
+	cout << longestValidParentheses("(()") << endl;
 	return 0;
 }
