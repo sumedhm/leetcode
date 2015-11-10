f13347739a7bf5798cb41948a907cb5867b668ce
sumedhm <sumedhm18@gmail.com>
2015-11-10 22:52:50 +0530

    100. Same BST

--- codes/recoverbst.cpp
+++ codes/recoverbst.cpp
@@ -2,27 +2,29 @@
 
 using namespace std;
 
-void find(TreeNode *root, TreeNode *pre, TreeNode* first, TreeNode *second){
+TreeNode *pre, *first, *second;
+void find(TreeNode *root){
 	if(root==NULL) return;
-	find(root->left, pre, first, second);
+	find(root->left);
 	if(pre==NULL) pre = root;
 	else {
 		if(pre->val > root->val){
 			if(first==NULL) first = pre;
 			second = root;
 		}
+		pre = root;
 	}
-	find(root->right, pre, first, second);
+	find(root->right);
 	return;
 }
 
 void recoverTree(TreeNode* root) {
 	if(root==NULL) return;
-	TreeNode *pre = NULL;
-	TreeNode *first = NULL;
-	TreeNode *second = NULL;
+	pre = NULL;
+	first = NULL;
+	second = NULL;
 	int tmp;
-	find(root, pre, first, second);
+	find(root);
 	tmp = first->val;
 	first->val = second->val;
 	second->val = tmp;
